072c4605862f62f96a4f6b5b6e006591b18fa5e6
akilinjamam <akilinjamam@gmail.comm>
2022-04-28 15:24:16 +0600

    updated

---
+++ __server_deploy.js
@@ -0,0 +1,18 @@
+/* 
+------------------------------
+*ONE TIME
+------------------------------
+* 1 create heroku account
+* 2 verify email
+* 3 install heroku account
+* 4 heroku login
+
+----------------------------------
+for each project one time
+----------------------------------
+* 1 heroku create
+
+
+
+
+*/
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 app.listen(port, () => {
     console.log('listening to port', port)
@@ -12,6 +13,28 @@ app.listen(port, () => {
 // middleware
 app.use(cors())
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+
+    const authHeder = req.headers.authorization
+    if (!authHeder) {
+        return res.status(401).send({ message: 'unauthorize access' });
+    }
+    const token = authHeder.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next()
+
+    })
+
+
+
+}
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
@@ -27,8 +50,20 @@ async function run() {
 
     try {
         await client.connect();
-        const serviceCollection = client.db('geniusCar').collection('service')
+        const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order')
 
+        // Auth  
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accesToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ accesToken })
+
+        })
+
+
+
+        // service api
         app.get('/service', async (req, res) => {
 
             const query = {};
@@ -38,6 +73,14 @@ async function run() {
         })
 
         app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+        })
+
+        app.get('/checkout/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
@@ -56,6 +99,31 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const result = await serviceCollection.deleteOne(query)
             res.send(result)
+        })
+
+        // order collection API
+
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email
+            if (email === decodedEmail) {
+                console.log(email)
+                const query = { email: email }
+                const cursor = orderCollection.find(query)
+                const orders = await cursor.toArray()
+                res.send(orders)
+            }
+
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+
         })
     }
     finally {
